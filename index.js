7e0efcd729dc023d195e1cd192bb1c5628182783
tamatar <>
2021-02-22 12:20:12 +0530

    process.env port added dynamically to fix heroku err

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get("*", (req, res) => {
   res.send("connection established !");
